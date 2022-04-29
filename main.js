a01c2831e4e844aa831c962ae912de4df6ab6618
tanembasaraner <59879129+tanembasaraner@users.noreply.github.com>
2022-04-29 18:24:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const port = 3000,
  express = require("express"),
  app = express();
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //GET REQUEST
+  console.log(req.params);
+console.log(req.body);
+console.log(req.url);
+console.log(req.query);
  res.send("Hello, Prof!");
 })
 .listen(port, () => {
