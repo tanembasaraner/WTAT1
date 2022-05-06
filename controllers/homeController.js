541aa63d042c4e609162b227e470669e988b6377
tanembasaraner <59879129+tanembasaraner@users.noreply.github.com>
2022-05-06 19:11:14 +0200

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,23 +2,23 @@
 
 
 exports.showSignUp = (req, res) => {
- res.render("contact");
+ res.render("login");
 };
 exports.postedSignUpForm = (req, res) => {
  res.render("thanks");
 };
 var packages = [
  {
- title: "Event Driven Cakes",
- cost: 50
+ title: "Greece",
+ cost: 500
  },
  {
- title: "Asynchronous Artichoke",
- cost: 25
+ title: "Thailand",
+ cost: 2500
  },
  {
- title: "Object Oriented Orange Juice",
- cost: 10
+ title: "Turkey",
+ cost: 1000
  }
 ];
 exports.showPackages = (req, res) => {
