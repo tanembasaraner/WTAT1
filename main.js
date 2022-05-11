ff32e51b48dcda5e1287c40001b81ffe16b362fb
tanembasaraner <59879129+tanembasaraner@users.noreply.github.com>
2022-05-11 16:54:59 +0200

    Lesson 13
    -setup mongodb
    -insert contact through the application and /or terminal

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const homeController = require("./controllers/homeController.js");
 const errorController = require("./controllers/errorController.js");
 
+const MongoDB = require("mongodb").MongoClient,
+ dbURL = "mongodb://localhost:27017",
+ dbName = "holidays_db";
+MongoDB.connect(dbURL, (error, client) => {
+ if (error) throw error;
+ let db = client.db(dbName);
+ db.collection("contacts")
+ .find()
+ .toArray((error, data) => {
+if (error) throw error;
+console.log(data);
+db.collection("contacts").insert(
+     {
+       name: "Freddie Mercury",
+       email: "fred@queen.com"
+     },
+     (error, db) => {
+       if (error) throw error;
+       console.log(db);
+     }
+   );
+ }
+);
+
+
 const express = require("express"),
 layouts = require("express-ejs-layouts"),
  app = express();
