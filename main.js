2a991dbd00f250de45eeef5b0b3835f03e51c242
StepanBurlachenko <58396989+StepanBurlachenko@users.noreply.github.com>
2022-06-27 15:00:28 +0200

    Lessons 24 and 25

--- main.js
+++ main.js
@@ -1,49 +1,25 @@
-const homeController = require("./controllers/homeController.js");
-const errorController = require("./controllers/errorController.js");
-const subscribersController = require("./controllers/subscribersController.js");
-const travelPackagesController = require("./controllers/travelPackagesController.js");
-const usersController = require("./controllers/usersController");
-const TravelPackage = require("./models/travelPackage.js")
-const Agent = require("./models/agent.js")
-const express = require("express");
-const layouts = require("express-ejs-layouts");
-const app = express();
-const router = express.Router();
-const methodOverride = require("method-override");
-const expressValidator = require("express-validator");
+"use strict";
 
-const expressSession = require("express-session"),
+const express = require("express"),
+  app = express(),
+  router = express.Router(),
+  layouts = require("express-ejs-layouts"),
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override"),
+  expressSession = require("express-session"),
   cookieParser = require("cookie-parser"),
-  connectFlash = require("connect-flash");
+  connectFlash = require("connect-flash"),
+  expressValidator = require("express-validator"),
+  passport = require("passport"),
+  homeController = require("./controllers/homeController.js"),
+  errorController = require("./controllers/errorController.js"),
+  subscribersController = require("./controllers/subscribersController.js"),
+  travelPackagesController = require("./controllers/travelPackagesController.js"),
+  usersController = require("./controllers/usersController"),
+  TravelPackage = require("./models/travelPackage.js"),
+  Agent = require("./models/agent.js"),
+  User = require("./models/user");
 
-router.use(
-  methodOverride("_method", {
-    methods: ["POST", "GET"]
-  })
-);
-
-router.use(express.json());
-router.use(cookieParser("secret_passcode"));
-router.use(
-  expressSession({
-    secret: "secret_passcode",
-    cookie: {
-      maxAge: 4000000
-    },
-    resave: false,
-    saveUninitialized: false
-  })
-);
-router.use(connectFlash());
-
-router.use((req, res, next) => {
-  res.locals.flashMessages = req.flash();
-  next();
-});
-
-router.use(expressValidator());
-
-const mongoose = require("mongoose");//set up mongoose
 mongoose.connect(
   "mongodb://localhost:27017/trays_travels",
   { useNewUrlParser: true }
@@ -66,6 +42,40 @@ db.once("open", () => {
   console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
+router.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+router.use(express.json());
+router.use(cookieParser("secret_passcode"));
+router.use(
+  expressSession({
+    secret: "secret_passcode",
+    cookie: {
+      maxAge: 4000000
+    },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+router.use(passport.initialize());
+router.use(passport.session());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+router.use(connectFlash());
+
+router.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+router.use(expressValidator());
+router.use(homeController.logRequestPaths);
 
 app.get("/users", usersController.index, usersController.indexView);
 app.get("/packages", travelPackagesController.index, travelPackagesController.indexView);
@@ -75,6 +85,7 @@ router.get("/users/new", usersController.new);
 router.post("/users/create", usersController.validate, usersController.create, usersController.redirectView);
 router.get("/users/login", usersController.login);
 router.post("/users/login", usersController.authenticate, usersController.redirectView);
+router.get("/users/logout", usersController.logout, usersController.redirectView);
 router.get("/users/:id/edit", usersController.edit);
 router.put("/users/:id/update", usersController.update, usersController.redirectView);
 router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
@@ -97,6 +108,7 @@ router.get("/subscribers/:id", subscribersController.show, subscribersController
 app.get("/", (req, res) => {
   res.send("Welcome to TRAYS Travels!");
 });
+
 
 // app.get("/contact", subscribersController.getSubscriptionPage);
 // app.post("/subscribe", subscribersController.saveSubscriber);
