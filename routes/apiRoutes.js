25f5d2a28bc155f15a3f2e6954c5ed5c861f8d2f
StepanBurlachenko <58396989+StepanBurlachenko@users.noreply.github.com>
2022-07-08 22:16:50 +0200

    Lesson 29

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,14 +9,13 @@ router.post("/login", usersController.apiAuthenticate);
 router.use(usersController.verifyJWT);
 
 
-
-router.get("/travelPackages/:id/join", travelPackagesController.join, travelPackagesController.respondJSON);
 router.get(
   "/travelPackages",
   travelPackagesController.index,
   travelPackagesController.filterUserTravelPackages,
   travelPackagesController.respondJSON
 );
+router.get("/travelPackages/:id/join", travelPackagesController.join, travelPackagesController.respondJSON);
 router.use(travelPackagesController.errorJSON);
 
 module.exports = router;
