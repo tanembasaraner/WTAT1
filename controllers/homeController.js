2164318da49e03c23abe04b5a57651916614cccf
StepanBurlachenko <58396989+StepanBurlachenko@users.noreply.github.com>
2022-07-15 21:52:14 +0200

    Lessons 30-33

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,7 +4,10 @@ module.exports = {
   index: (req, res) => {
     res.render("index");
   },
-	contact: (req, res) => {
-		res.render("contact");
-	}
+  contact: (req, res) => {
+    res.render("contact");
+  },
+  chat: (req, res) => {
+    res.render("chat");
+  }
 };
--- public/js/recipeApp.js
+++ public/js/recipeApp.js
@@ -1,4 +1,57 @@
 $(document).ready(() => {
+  const socket = io();
+  $("#chatForm").submit(() => {
+    let text = $("#chat-input").val(),
+      userName = $("#chat-user-name").val(),
+      userId = $("#chat-user-id").val();
+
+    socket.emit("message", {
+      content: text,
+      userName: userName,
+      userId: userId
+    });
+
+    $("#chat_input").val("");
+    return false;
+  });
+
+  socket.on("message", message => {
+    displayMessage(message);
+    for (let i = 0; i < 2; i++) {
+      $(".chat-icon")
+        .fadeOut(200)
+        .fadeIn(200);
+    }
+  });
+
+  socket.on("load all messages", data => {
+    data.forEach(message => {
+      displayMessage(message);
+    });
+  });
+
+  socket.on("user disconnected", () => {
+    displayMessage({
+      userName: "Notice",
+      content: "User left the chat"
+    });
+  });
+
+  let displayMessage = message => {
+    $("#chat").prepend(
+      $("<li>").html(`
+				<strong class="message ${getCurrentUserClass(message.user)}">
+					${message.userName}
+				</strong>: ${message.content}
+			`)
+    );
+  };
+
+  let getCurrentUserClass = id => {
+    let userId = $("#chat-user-id").val();
+    return userId === id ? "current-user" : "";
+  };
+
   $("#modal-button").click(() => {
     $(".modal-body").html("");
     $.get("/api/travelPackages", (results = {}) => {
@@ -28,7 +81,7 @@ $(document).ready(() => {
 let addJoinButtonListener = () => {
   $(".join-button").click(event => {
     let $button = $(event.target),
-    travelPackageId = $button.data("id");
+      travelPackageId = $button.data("id");
     $.get(`/api/travelPackages/${travelPackageId}/join`, (results = {}) => {
       let data = results.data;
       if (data && data.success) {
